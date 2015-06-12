e3131d86f03929182fdd29c2569405f288458118
maiconio <maiconscosta@gmail.com>
2015-06-11 23:20:18 -0300

    movendo funções léxicas para um arquivo separado

---
+++ lexico.go
@@ -0,0 +1,29 @@
+package main
+
+import "regexp"
+
+func verificaInteiro(token string) bool {
+	matched, _ := regexp.MatchString("^[0-9]+$", token)
+	return matched
+}
+
+func verificaReal(token string) bool {
+	matched, _ := regexp.MatchString("^[0-9]+,[0-9]+$", token)
+	return matched
+}
+
+func verificaCaractere(token string) bool {
+	matched, _ := regexp.MatchString("^\".*\"", token)
+	return matched
+}
+
+func verificaLogico(token string) bool {
+	matched, _ := regexp.MatchString("^(verdadeiro|falso)$", token)
+	return matched
+}
+
+func verificaVariavel(token string) bool {
+	matched, _ := regexp.MatchString("^[A-z][A-z0-9]+$", token)
+
+	return matched && !verificaLogico(token)
+}
--- main.go
+++ main.go
@@ -1,33 +1,5 @@
 package main
 
-import "regexp"
-
 func main() {
 
-}
-
-func verificaInteiro(token string) bool {
-	matched, _ := regexp.MatchString("^[0-9]+$", token)
-	return matched
-}
-
-func verificaReal(token string) bool {
-	matched, _ := regexp.MatchString("^[0-9]+,[0-9]+$", token)
-	return matched
-}
-
-func verificaCaractere(token string) bool {
-	matched, _ := regexp.MatchString("^\".*\"", token)
-	return matched
-}
-
-func verificaLogico(token string) bool {
-	matched, _ := regexp.MatchString("^(verdadeiro|falso)$", token)
-	return matched
-}
-
-func verificaVariavel(token string) bool {
-	matched, _ := regexp.MatchString("^[A-z][A-z0-9]+$", token)
-
-	return matched && !verificaLogico(token)
 }
