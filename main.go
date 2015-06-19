d57759661533722500443aa8743070535d0d36be
maiconio <maiconscosta@gmail.com>
2015-06-19 18:23:09 -0300

    adicionando bases para leitura de flags e arquivos

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+)
 
+func main() {
+	flag.Parse()
+	arquivo := flag.Arg(0)
+
+	if len(arquivo) > 0 {
+		bufferArquivo, err := ioutil.ReadFile(arquivo)
+
+		if err == nil {
+			fmt.Printf("%v\n", string(bufferArquivo))
+		} else {
+			fmt.Printf("Erro ao abrir o arquivo: %v\n", err)
+		}
+	}
 }
